3f56b80df0cc4f4ed947b4bc9531eea1ebf33dc5
vinilauter <vinnielauter@gmail.com>
2025-12-27 08:56:18 -0300

    feat: tratamento de dados e colunas

---
+++ load_data.py
@@ -0,0 +1,39 @@
+import gspread 
+import pandas as pd
+
+# client auth
+cliente = gspread.service_account(filename = './service_account.json')
+
+# open sheet
+sheet = cliente.open("Controle Financeiro  (respostas)")
+
+# open worksheet
+worksheet = sheet.sheet1
+
+# get data
+brute_data = worksheet.get_all_records()
+
+# create dataframe
+dataframe = pd.DataFrame(brute_data)
+
+print(dataframe.columns)
+
+# rename columns for better coding
+dataframe = dataframe.rename(columns={"Carimbo de data/hora": "Data Atual", "Natureza da Operação": "Operacao", "Forma de Pagamento": "Meio", "Data": "Data Manual"})
+
+# money types cast
+dataframe["Valor"] = dataframe["Valor"].astype(str).str.replace(',', '.')
+dataframe["Valor"] = pd.to_numeric(dataframe["Valor"], downcast='float', errors='coerce')
+
+# date types cast
+dataframe["Data Atual"] = pd.to_datetime(dataframe["Data Atual"], dayfirst=True)
+dataframe["Data Manual"] = pd.to_datetime(dataframe["Data Manual"], dayfirst=True, errors="coerce")
+
+# final date logic
+dataframe["Data Final"] = dataframe["Data Manual"].fillna(dataframe["Data Atual"])
+
+print("--- PRIMEIROS REGISTROS ---")
+print(dataframe.head())
+
+print("\n--- INFORMAÇÕES DAS COLUNAS ---")
+print(dataframe.info())
